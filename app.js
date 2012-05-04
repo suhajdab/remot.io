37bb300de7dcd5f2b052edca57e4d4d9071c7c41
Balázs Suhajda <balazs@onereason.eu>
2012-05-05 00:08:38 +0200

    h5bp to express in .ejs

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
