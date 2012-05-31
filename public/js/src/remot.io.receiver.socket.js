de05c051f18fb8e99b72513eab9e4992ae0f6087
Balázs Suhajda <balazs@onereason.eu>
2012-05-31 17:28:58 +0200

    inline docs, handling local debugging, setting host on prod

--- public/js/src/remot.io.receiver.socket.js
+++ public/js/src/remot.io.receiver.socket.js
@@ -1,10 +1,17 @@
 /**
- * TODO: set host to remot.io
  */
 
 (function(){
 	'use strict'
 
-	if (!io) throw ( 'socketio not found' );
-	remot.io.socket = io.connect( '//' + location.host + '/receiver', { host: 'remot.io'} );
+	var opts = {};
+
+	//	if we don't have io, the party is over
+	if ( !io ) throw ( 'socketio not found' );
+
+	//	if we're not local debugging, io host should always be remot.io
+	if ( location.host != 'onembp.local' ) opts.host = 'remot.io';
+
+	//	set up receiver socket
+	remot.io.socket = io.connect( '//' + location.host + '/receiver', opts );
 })();
